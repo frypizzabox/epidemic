565edc3ea66d40c82448e33e0b7174f3388cd8da
Adalberto Aucar Kutuxidis <adalberto.kutuxidis@gmail.com>
2025-01-19 19:48:38 +0100

    Add BE implementation regarding Playlists

--- service/api/models.py
+++ service/api/models.py
@@ -48,3 +48,14 @@ class Track(models.Model):
     @property
     def spotify(self):
         return "{}{}/{}".format(settings.DSP_BASE, self.id, "spotify")
+
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    tracks = models.ManyToManyField(Track, related_name="playlists")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
